d90cad28d08f5ca8311fd64ce2c30d148a8936e0
Dom Smith <dom@teneightfive.com>
2014-01-31 15:53:45 +0000

    Restructure build process to be more robust

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+  'use strict';
 
   // Project configuration.
   grunt.initConfig({
@@ -13,20 +14,34 @@ module.exports = function(grunt) {
       root: '../',
       src: '<%= dirs.root %>src/',
       dist: '<%= dirs.root %>dist/',
-      govuk_template: '<%= dirs.root %>../govuk_template/source/assets/',
+      build: '<%= dirs.root %>build/',
+      temp: '<%= dirs.build %>temp/',
+      govuk_template: '<%= dirs.root %>../govuk_template/',
+      govuk_template_assets: '<%= dirs.govuk_template %>source/assets/',
       govuk_toolkit: '<%= dirs.root %>../govuk_frontend_toolkit/',
-      moj_boilerplate: '<%= dirs.root %>../moj_boilerplate/app/assets/'
+      moj_boilerplate: '<%= dirs.root %>../moj_boilerplate/'
     },
 
     // Task configuration.
     concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+      govuk_toolkit: {
+        src: [
+          '<%= dirs.temp %>govuk_toolkit/javascripts/vendor/**/*.js',
+          '<%= dirs.temp %>govuk_toolkit/javascripts/govuk/*.js',
+          '<%= dirs.temp %>govuk_toolkit/javascripts/*.js'
+        ],
+        dest: '<%= dirs.dist %>javascripts/govuk-toolkit.js'
       },
-      dist: {
-        src: ['<%= dirs.src %>javascripts/**/*.js'],
-        dest: '<%= dirs.dist %>javascripts/<%= pkg.name %>.js'
+      moj_boilerplate: {
+        options: {
+          banner: '<%= banner %>',
+          stripBanners: true
+        },
+        src: [
+          '<%= dirs.temp %>moj_boilerplate/javascripts/moj.js',
+          '<%= dirs.temp %>moj_boilerplate/javascripts/**/*.js'
+        ],
+        dest: '<%= dirs.dist %>javascripts/moj.js'
       }
     },
 
@@ -34,80 +49,123 @@ module.exports = function(grunt) {
       options: {
         banner: '<%= banner %>'
       },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: '<%= dirs.dist %>javascripts/<%= pkg.name %>.min.js'
+      govuk_toolkit: {
+        src: '<%= concat.govuk_toolkit.dest %>',
+        dest: '<%= dirs.dist %>javascripts/govuk-toolkit.min.js'
+      },
+      moj_boilerplate: {
+        src: '<%= concat.moj_boilerplate.dest %>',
+        dest: '<%= dirs.dist %>javascripts/moj.min.js'
       }
     },
 
     clean: {
-      src: {
-        options: {
-          force: true
-        },
-        src: ['<%= dirs.src %>*', '!.gitkeep']
+      options: {
+        force: true
       },
-      dist: {
-        options: {
-          force: true
-        },
-        src: ['<%= dirs.dist %>*', '!.gitkeep']
+      prebuild: {
+        src: [
+          '<%= dirs.src %>*',
+          '<%= dirs.dist %>*',
+          '<%= dirs.temp %>',
+          '!.gitkeep'
+        ]
+      },
+      postbuild: {
+        src: [
+          '<%= dirs.temp %>'
+        ]
+      }
+    },
+
+    replace: {
+      govuk_template: {
+        src: ['<%= dirs.temp %>govuk_template/views/layouts/govuk_template.html'],
+        overwrite: true,
+        replacements: [{
+          from: '{{ assetPath }}',
+          to: '{{{ assetPath }}}'
+        }]
+      }
+    },
+
+    mustache_render: {
+      govuk_template: {
+        files: [{
+          data: {
+            assetPath: './',
+            head: grunt.file.read('moj-template/partials/head.mustache'),
+            cookieMessage: grunt.file.read('moj-template/partials/cookie-message.mustache'),
+            footerTop: grunt.file.read('moj-template/partials/footer-top.mustache'),
+            bodyEnd: grunt.file.read('moj-template/partials/body-end.mustache')
+          },
+          template: '<%= replace.govuk_template.src %>',
+          dest: '<%= dirs.dist %>moj_template.html'
+        }]
       }
     },
 
     copy: {
-      scripts: {
+      govuk_mustache: {
         files: [
           {
             expand: true,
-            cwd: '<%= dirs.govuk_template %>',
-            src: ['javascripts/**'],
-            dest: '<%= dirs.src %>'
-          },
-          {
-            expand: true,
-            cwd: '<%= dirs.govuk_toolkit %>',
-            src: ['javascripts/**'],
-            dest: '<%= dirs.src %>'
-          },
-          {
-            expand: true,
-            cwd: '<%= dirs.moj_boilerplate %>',
-            src: ['javascripts/**'],
-            dest: '<%= dirs.src %>'
+            cwd: '<%= dirs.govuk_template %>pkg/',
+            src: ['mustache_govuk_template-*/**'],
+            dest: '<%= dirs.temp %>/',
+            rename: function (dest, src) {
+              var path = require('path'),
+                  component = src.split(path.sep).slice(0, 1)[0];
+
+              return dest + src.replace(component, 'govuk_template');
+            }
           }
         ]
       },
-      styles: {
+      govuk_assets: {
         files: [
           {
             expand: true,
-            cwd: '<%= dirs.govuk_template %>',
-            src: ['stylesheets/**'],
-            dest: '<%= dirs.src %>'
-          },
-          {
-            expand: true,
-            cwd: '<%= dirs.govuk_toolkit %>',
-            src: ['stylesheets/**'],
-            dest: '<%= dirs.src %>'
-          },
-          {
-            expand: true,
-            cwd: '<%= dirs.moj_boilerplate %>',
-            src: ['stylesheets/**'],
-            dest: '<%= dirs.src %>'
+            cwd: '<%= dirs.temp %>govuk_template/assets/',
+            src: ['**'],
+            dest: '<%= dirs.dist %>'
           }
         ]
       },
-      images: {
+      moj_partials: {
         files: [
           {
             expand: true,
-            cwd: '<%= dirs.govuk_template %>',
-            src: ['images/**'],
-            dest: '<%= dirs.dist %>'
+            cwd: '<%= dirs.moj_boilerplate %>app/views/layouts/partials/',
+            src: ['**'],
+            dest: '<%= dirs.temp %>/moj_boilerplate/partials/'
+          }
+        ]
+      },
+      sass: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= dirs.govuk_template %>app/assets/stylesheets/',
+            src: ['**'],
+            dest: '<%= dirs.src %>sass/'
           },
+          {
+            expand: true,
+            cwd: '<%= dirs.govuk_toolkit %>stylesheets/',
+            src: ['**'],
+            dest: '<%= dirs.src %>sass/'
+          },
+          {
+            expand: true,
+            cwd: '<%= dirs.moj_boilerplate %>app/assets/stylesheets/',
+            src: ['**'],
+            dest: '<%= dirs.src %>sass/'
+          }
+        ]
+      },
+      govuk_frontend_toolkit: {
+        files: [
           {
             expand: true,
             cwd: '<%= dirs.govuk_toolkit %>',
@@ -116,9 +174,25 @@ module.exports = function(grunt) {
           },
           {
             expand: true,
-            cwd: '<%= dirs.moj_boilerplate %>',
+            cwd: '<%= dirs.govuk_toolkit %>',
+            src: ['javascripts/**'],
+            dest: '<%= dirs.temp %>govuk_toolkit/'
+          }
+        ]
+      },
+      moj_boilerplate: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= dirs.moj_boilerplate %>app/assets/',
             src: ['images/**'],
             dest: '<%= dirs.dist %>'
+          },
+          {
+            expand: true,
+            cwd: '<%= dirs.moj_boilerplate %>app/assets/',
+            src: ['javascripts/**'],
+            dest: '<%= dirs.temp %>moj_boilerplate/'
           }
         ]
       }
@@ -126,13 +200,22 @@ module.exports = function(grunt) {
 
     sass: {
       dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= dirs.src %>stylesheets',
-          src: ['*.scss'],
-          dest: '<%= dirs.dist %>stylesheets',
-          ext: '.css'
-        }]
+        files: {
+          '<%= dirs.dist %>stylesheets/moj.css': '<%= dirs.src %>sass/moj-base.scss'
+        }
+      }
+    },
+
+    haml: {
+      moj_partials: {
+        files: {
+          '<%= dirs.temp %>moj_boilerplate/partials/_content.html': '<%= dirs.temp %>moj_boilerplate/partials/_content.html.haml'
+        }
+        // files: grunt.file.expandMapping(['<%= dirs.temp %>moj_boilerplate/partials/*.haml'], '<%= dirs.temp %>moj_boilerplate/haml/', {
+        //   rename: function(base, path) {
+        //     return base + path.replace(/\.haml$/, '.js');
+        //   }
+        // })
       }
     }
   });
@@ -143,9 +226,29 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  // grunt.loadNpmTasks('grunt-contrib-haml');
+  grunt.loadNpmTasks('grunt-mustache-render');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-haml');
 
   // Default task.
   grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['clean', 'copy', 'sass', 'concat', 'uglify']);
+  grunt.registerTask('build', [
+    'clean:prebuild',
+    'copy:govuk_mustache',
+    'replace:govuk_template',
+    'mustache_render:govuk_template',
+    'copy:govuk_assets',
+    'copy:moj_partials',
+    'copy:govuk_frontend_toolkit',
+    'copy:moj_boilerplate',
+    'copy:sass',
+    'sass',
+    'concat',
+    'uglify',
+    'clean:postbuild'
+  ]);
 
 };
+
+
